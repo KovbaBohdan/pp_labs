b7de14d45d187b50474abc7efea10be79e23d708
Bohdan <bohdan.kovba1812@gmail.com>
2024-09-06 10:01:51 +0300

    Complete tasks 3 and 4

---
+++ src/pp_lab1/lab1_task5.java
@@ -0,0 +1,25 @@
+package pp_lab1;
+
+import java.util.Scanner;
+
+public class lab1_task5 {
+    public static void main(String[] args) {
+        String [][] matrix = {
+                {"asd", "qwe", "fgh"},
+                {"123", "678", "sgd"},
+                {"5678", "qwef", "lkjh"}
+        };
+        String substring = "678";
+        //print(matrix);
+
+    }
+//    public static void print(){
+//        for(int i = 0; i < matrix.length; i++) {
+//            for(int j = 0; j < matrix[i].length; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+}
