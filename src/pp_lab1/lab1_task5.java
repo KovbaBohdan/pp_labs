7af483197ffc2f5d9cfca640542a7fe3480f136d
Bohdan <bohdan.kovba1812@gmail.com>
2024-09-08 21:10:12 +0300

    Complete task 5. Adding 6 task

--- src/pp_lab1/lab1_task5.java
+++ src/pp_lab1/lab1_task5.java
@@ -7,19 +7,37 @@ public class lab1_task5 {
         String [][] matrix = {
                 {"asd", "qwe", "fgh"},
                 {"123", "678", "sgd"},
-                {"5678", "qwef", "lkjh"}
+                {"678910", "qwef", "lk678jh"}
         };
+        print(matrix);
         String substring = "678";
-        //print(matrix);
+        int count_of_substring = count_substring(matrix, substring);
+        System.out.println(count_of_substring);
 
     }
-//    public static void print(){
-//        for(int i = 0; i < matrix.length; i++) {
-//            for(int j = 0; j < matrix[i].length; j++) {
-//                System.out.print(matrix[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//
-//    }
+
+    public static int count_substring(String [][] matrix, String substring){
+        int count = 0;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                String current_String = matrix[i][j];
+                int index = current_String.indexOf(substring);
+                while (index != -1) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void print(String [][] matrix){
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
---
+++ src/pp_lab1/task6/lab1_task1.java
@@ -0,0 +1,4 @@
+package pp_lab1.task6;
+
+public class lab1_task1 {
+}
